d97743d6fb658ff780193cd90646912213505f0a
SAIKAT0604 <saikats.kolkata06@gmail.com>
2020-01-27 14:03:27 +0530

    first commit

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float Yincrement, speed, maxheight, minheight;
+    public int health;
+    public GameObject effect;
+    public Text healthDisplay;
+    public GameObject gameOver;
+    private Vector3 fp;   //First touch position
+    private Vector3 lp;   //Last touch position
+    private float dragDistance;  //minimum distance for a swipe to be registered
+
+    void Start()
+    {
+        dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                fp = touch.position;
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                lp = touch.position;  //last touch position. Ommitted if you use list
+
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if ((lp.x > fp.x))  //If the movement was to the right)
+                        {   //Right swipe
+                            Debug.Log("Right Swipe");
+                        }
+                        else
+                        {   //Left swipe
+                            Debug.Log("Left Swipe");
+                        }
+                    }
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (lp.y > fp.y)  //If the movement was up
+                        {   //Up swipe
+                            Debug.Log("Up Swipe");
+                            if (transform.position.y < maxheight)
+                            {
+                                Instantiate(effect, transform.position, Quaternion.identity);
+                                targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+                            }
+                        }
+                        else
+                        {   //Down swipe
+                            Debug.Log("Down Swipe");
+                            if(transform.position.y > minheight)
+                            {
+                                Instantiate(effect, transform.position, Quaternion.identity);
+                                targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement); }
+                        }
+                    }
+                }
+                else
+                {   //It's a tap as the drag distance is less than 20% of the screen height
+                    Debug.Log("Tap");
+                }
+            }
+        }
+        healthDisplay.text = health.ToString();
+        if(health<=0)
+        {
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxheight)
+        {
+            
+            Instantiate(effect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minheight)
+        {
+            Instantiate(effect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+    }
+}
